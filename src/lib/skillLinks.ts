6250611c7711584c7cc00083b3ed0c1d4290821e
lvpjsdev <lvp@lvpjsdev.com>
2026-05-05 13:00:11 +0300

    feat: SkillTag clickable links + SkillIcon component with technology icons

---
+++ src/lib/skillLinks.ts
@@ -0,0 +1,12 @@
+export const skillLinks: Record<string, string> = {
+  Astro: 'https://docs.astro.build',
+  TypeScript: 'https://www.typescriptlang.org/docs/',
+  CSS: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+  React: 'https://react.dev/',
+  'Node.js': 'https://nodejs.org/docs',
+  PostgreSQL: 'https://www.postgresql.org/docs/',
+  JavaScript: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+  HTML: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+  Git: 'https://git-scm.com/docs',
+  Terrazzo: 'https://localhost:3000',
+};
